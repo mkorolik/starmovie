9dd25ded3d4cf83dc041beaef5224fe56201c110
mkorolik <mariakorolik21@mittymonarch.com>
2025-04-23 17:24:38 -0400

    plot_hr size dot by radius, color dot by dominant nuclear reaction

--- starmovie.py
+++ starmovie.py
@@ -41,6 +41,23 @@ rgbs = [(1, 1, 1), # X, temp class just for setting upper bound
 
 solar_mass_g = 1.9885 * 10**33
 
+rxns = ['pp', 'cno', 'tri_alpha', 'c_alpha', 'n_alpha', 'o_alpha', 'ne_alpha', 'na_alpha', 'mg_alpha', 'si_alpha', 's_alpha',
+         'ar_alpha', 'ca_alpha', 'ti_alpha', 'fe_co_ni', 'c12_c12', 'c12_o16', 'o16_o16']
+
+# cmap1 = mpl.colormaps['Set1']
+# cmap2 = mpl.colormaps['Set2']
+
+# colors1 = cmap1(np.linspace(0,1, int(len(rxns)/2))) 
+# colors2 = cmap2(np.linspace(0,1, int(len(rxns)/2)))
+# rxn_colors = np.concat((colors1,colors2))
+
+rxn_colors = ['darkred', 'tomato', 'limegreen', 'teal', 'dodgerblue', 'darkblue', 'darkorchid', 'purple', 'magenta',
+               'pink', 'pink', 'pink', 'pink', 'pink', 'pink', 'pink', 'pink', 'pink']
+
+
+
+
+
 
 def plot_colors(y, xlim=[30000, 0], ax=None, alpha=0.5):
     if ax is None:
@@ -61,8 +78,7 @@ def plot_colors(y, xlim=[30000, 0], ax=None, alpha=0.5):
 
 
 
-
-def plot_HR(DF, idx=None, fig=None, ax=None, xlim=[10000, 3500], ylim=[0.1, 10000], linecolor='black', dotcolor='red', age_plot=False):
+def plot_HR(DF, idx=None, fig=None, ax=None, xlim=[10000, 3500], ylim=[0.1, 10000], linecolor='black', dotcolor='red', age_plot=False, bysize=True, byrxn=True):
     # if fig is not None:
         # fig.clf()
 
@@ -79,7 +95,16 @@ def plot_HR(DF, idx=None, fig=None, ax=None, xlim=[10000, 3500], ylim=[0.1, 1000
     ax.plot(10**DF['log_Teff'], 10**DF['log_L'], lw=1, color=linecolor)
 
     if idx is not None:
-        ax.scatter(10**DF.iloc[idx]['log_Teff'], 10**DF.iloc[idx]['log_L'], color=dotcolor, zorder=100)
+        if bysize is False:
+            s=20
+        if bysize is True:
+            s=10*10**DF.iloc[idx]['log_R']
+        if byrxn is True:
+            # rxns[np.argmax(DF.iloc[a][rxns])]
+            dotcolor = rxn_colors[np.argmax(DF.iloc[idx][rxns])]
+
+
+        ax.scatter(10**DF.iloc[idx]['log_Teff'], 10**DF.iloc[idx]['log_L'], color=dotcolor, zorder=100, s=s)
 
     ax.set_yscale('log')
     ax.set_ylim(ylim)
@@ -90,7 +115,7 @@ def plot_HR(DF, idx=None, fig=None, ax=None, xlim=[10000, 3500], ylim=[0.1, 1000
 
 
 
-def plot_convection_circles(DF, idx, fig=None, base_color=None, ax=None, age_plot=False, age_scale=10**-9, age_label='Gyr'):
+def plot_convection_circles(DF, idx, fig=None, base_color=None, ax=None, age_plot=False, age_scale=10**-9, age_label='Gyr', originalburnplot=False):
     # if fig is not None:
         # fig.clf()
         
@@ -128,21 +153,29 @@ def plot_convection_circles(DF, idx, fig=None, base_color=None, ax=None, age_plo
 
 
     base = plt.Circle((0,0), mass, color=base_color)
-    conv_core = plt.Circle((0,0), df['mass_conv_core'], color='firebrick')
-    conv1_top = plt.Circle((0,0), df['conv_mx1_top'] * mass, color='firebrick')
+    conv_core = plt.Circle((0,0), df['mass_conv_core'], color='gray')
+    conv1_top = plt.Circle((0,0), df['conv_mx1_top'] * mass, color='gray')
     conv1_bot = plt.Circle((0,0), df['conv_mx1_bot'] * mass, color=base_color)
-    conv2_top = plt.Circle((0,0), df['conv_mx2_top'] * mass, color='firebrick')
+    conv2_top = plt.Circle((0,0), df['conv_mx2_top'] * mass, color='gray')
     conv2_bot = plt.Circle((0,0), df['conv_mx2_bot'] * mass, color=base_color)
 
-    h_check = False
-    if df['epsnuc_M_5'] != -20:
-        h = mpl.patches.Annulus((0,0), df['epsnuc_M_8'] / solar_mass_g, (df['epsnuc_M_8'] - df['epsnuc_M_5']) / solar_mass_g, hatch='\\\\', alpha=0)
-        h_check=True
-    
-    he_check=False
-    if df['epsnuc_M_1'] != -20:
-        he = mpl.patches.Annulus((0,0), df['epsnuc_M_4']  / solar_mass_g, (df['epsnuc_M_4'] - df['epsnuc_M_1']) / solar_mass_g, hatch='////', alpha=0)
-        he_check=True
+    if originalburnplot:
+        h_check = False
+        if df['epsnuc_M_5'] != -20:
+            h = mpl.patches.Annulus((0,0), df['epsnuc_M_8'] / solar_mass_g, (df['epsnuc_M_8'] - df['epsnuc_M_5']) / solar_mass_g, hatch='\\\\', alpha=0)
+            h_check=True
+        
+        he_check=False
+        if df['epsnuc_M_1'] != -20:
+            he = mpl.patches.Annulus((0,0), df['epsnuc_M_4']  / solar_mass_g, (df['epsnuc_M_4'] - df['epsnuc_M_1']) / solar_mass_g, hatch='////', alpha=0)
+            he_check=True
+    else:
+        if df['burn_type_1'] == 0:
+            anyburning=False
+        else:
+            anyburning=True
+            burnpatch = mpl.patches.Annulus
+
    
     surface = mpl.patches.Annulus((0,0), mass + 0.1, 0.05, color=surface_color)
 
@@ -155,10 +188,14 @@ def plot_convection_circles(DF, idx, fig=None, base_color=None, ax=None, age_plo
 
     ax.add_patch(conv_core)
 
-    if h_check:
-        ax.add_patch(h)
-    if he_check:
-        ax.add_patch(he)
+    if originalburnplot:
+        if h_check:
+            ax.add_patch(h)
+        if he_check:
+            ax.add_patch(he)
+    
+    if anyburning:
+        ax.add_patch(burnpatch)
 
     ax.add_patch(surface)
 
@@ -211,7 +248,7 @@ def create_image_folder(history_file, plotfunction, folder_name='./images', star
         
 
 def create_video(image_folder, video_name='starmovie.avi', fps=15):
-    images = [img for img in sorted(os.listdir('./images'), key=lambda x: os.path.getmtime(os.path.join('./images', x))) if img.endswith((".jpg", ".jpeg", ".png"))]
+    images = [img for img in sorted(os.listdir(image_folder), key=lambda x: os.path.getmtime(os.path.join(image_folder, x))) if img.endswith((".jpg", ".jpeg", ".png"))]
     
     print("Images:", images)
     
